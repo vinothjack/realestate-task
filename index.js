d3e3834ae315884ecc68cfdbbe34e9b303aa1be9
Vinoth <106597409+vinothjack@users.noreply.github.com>
2025-01-18 22:18:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -46,11 +46,11 @@ app.use("/api/user", userRouter); //api/user
 app.use("/api/auth", authRouter); //api/auth
 app.use("/api/listing", listingRouter); //api/listing
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+// app.use(express.static(path.join(__dirname, "/client/dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+// });
 
 //Errors Handler
 app.use((err, req, res, next) => {
