db23d6c31827d37122200731baf5334982c95884
Vinoth <106597409+vinothjack@users.noreply.github.com>
2025-01-25 17:36:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const app = express();
 
 // CORS configuration
 const corsOptions = {
-  origin: 'https://friendly-wisp-12e43c.netlify.app', // Change this to your frontend URL
+  origin: 'https://stunning-eclair-e2ddce.netlify.app', // Change this to your frontend URL
   credentials: true, // Allow cookies to be sent
 };
 app.use(cors(corsOptions));
